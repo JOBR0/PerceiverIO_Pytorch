c6970970b4fd557b838eb0e6612757914fb8d4a0
Jonas <jonas.brokmeier@tum.de>
2022-01-19 00:11:05 +0100

    Add haiku_models download

---
+++ haiku_models/download_models.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#Optical Flow 
+wget https://storage.googleapis.com/perceiver_io/optical_flow_checkpoint.pystate
+
+#Image Classification
+wget https://storage.googleapis.com/perceiver_io/imagenet_conv_preprocessing.pystate
+wget https://storage.googleapis.com/perceiver_io/imagenet_fourier_position_encoding.pystate
+wget https://storage.googleapis.com/perceiver_io/imagenet_learned_position_encoding.pystate
