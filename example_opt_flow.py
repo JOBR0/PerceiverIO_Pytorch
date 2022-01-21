36c6d6357e55c8afe2e6b2397f559d1b7729adc1
Jonas <jonas.brokmeier@tum.de>
2022-01-22 00:58:16 +0100

    Update optical flow example

--- example_opt_flow.py
+++ example_opt_flow.py
@@ -1,3 +1,4 @@
+import os
 import torch
 
 from PIL import Image
@@ -6,22 +7,32 @@ import matplotlib.pyplot as plt
 
 from utils.flow_utils import flow_to_image
 from perceiver_io.flow_perceiver import FlowPerceiver
-
-
+from utils.utils import load_image
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-perceiver = FlowPerceiver()
+perceiver = FlowPerceiver(img_size=(368, 496))
 
-perceiver.load_haiku_params("./haiku_checkpoints/optical_flow_checkpoint.pystate")
+# check if file exists
+if not os.path.isfile("./pytorch_checkpoints/flow_perceiver_io.pth"):
+    raise ValueError("Please download the model checkpoint and place it in /pytorch_checkpoints")
+
+checkpoint = torch.load("./pytorch_checkpoints/flow_perceiver_io.pth", map_location=device)
+
+perceiver.load_state_dict(checkpoint['model_state_dict'])
 
 img1 = load_image("./sample_data/frame_0016.png", device)
 img2 = load_image("./sample_data/frame_0017.png", device)
 
+# Normalize images
+img1 = 2 * (img1 / 255.0) - 1.0
+img2 = 2 * (img2 / 255.0) - 1.0
+
+# Predict Flow
 with torch.inference_mode():
     flow = perceiver(img1, img2, test_mode=True)
 
-# Show prediciton
+# Show prediction
 figure = plt.figure()
 plt.subplot(2, 2, 1)
 plt.imshow(img1[0].permute(1, 2, 0).cpu().numpy()/255)
