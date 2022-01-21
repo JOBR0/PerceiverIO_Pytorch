fdb62c2ccea4038376f013ed89e0ddcf25be65b2
Jonas <jonas.brokmeier@tum.de>
2022-01-21 17:45:43 +0100

    Addd language haiku model

--- haiku_models/download_models.sh
+++ haiku_models/download_models.sh
@@ -6,3 +6,6 @@ wget https://storage.googleapis.com/perceiver_io/optical_flow_checkpoint.pystate
 wget https://storage.googleapis.com/perceiver_io/imagenet_conv_preprocessing.pystate
 wget https://storage.googleapis.com/perceiver_io/imagenet_fourier_position_encoding.pystate
 wget https://storage.googleapis.com/perceiver_io/imagenet_learned_position_encoding.pystate
+
+#Masked Language Modelling
+wget https://storage.googleapis.com/perceiver_io/language_perceiver_io_bytes.pickle
