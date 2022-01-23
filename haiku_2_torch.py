ebe374d868d7e7871281ebce86f28766ee1ad78f
Jonas <jonas.brokmeier@tum.de>
2022-01-23 22:35:46 +0100

    Update haiku_2_torch

--- haiku_2_torch.py
+++ haiku_2_torch.py
@@ -4,9 +4,9 @@ import torch
 
 from perceiver_io.language_perceiver import LanguagePerceiver
 from perceiver_io.flow_perceiver import FlowPerceiver
-from perceiver_io.classification_perceiver import ClassificationPerceiver
+from perceiver_io.classification_perceiver import ClassificationPerceiver, PrepType
 
-
+print("Language")
 model = LanguagePerceiver()
 haiku_file = "haiku_checkpoints/language_perceiver_io_bytes.pickle"
 model.load_haiku_params(haiku_file)
@@ -18,47 +18,49 @@ state_dicts = {
 }
 torch.save(state_dicts, os.path.join("pytorch_checkpoints", model_name))
 
+print("Flow")
 model = FlowPerceiver()
 haiku_file = "haiku_checkpoints/optical_flow_checkpoint.pystate"
 model.load_haiku_params(haiku_file)
 
-model_name = "flow_perceiver_io.pth"
+model_name = "optical_flow_checkpoint.pth"
 
 state_dicts = {
     "model_state_dict": model.state_dict()
 }
 torch.save(state_dicts, os.path.join("pytorch_checkpoints", model_name))
 
+print("Classification")
 model = ClassificationPerceiver()
 
 
-model = ClassificationPerceiver(prep_type="conv_preprocessing")
+model = ClassificationPerceiver(prep_type=PrepType.FOURIER_POS_CONVNET)
 haiku_file = "haiku_checkpoints/imagenet_conv_preprocessing.pystate"
 model.load_haiku_params(haiku_file)
 
-model_name = "classify_perceiver_io_conv_preprocessing.pth"
+model_name = "imagenet_conv_preprocessing.pth"
 
 state_dicts = {
     "model_state_dict": model.state_dict()
 }
 torch.save(state_dicts, os.path.join("pytorch_checkpoints", model_name))
 
-model = ClassificationPerceiver(prep_type="learned_pos_encoding")
+model = ClassificationPerceiver(prep_type=PrepType.LEARNED_POS_1X1CONV)
 haiku_file = "haiku_checkpoints/imagenet_learned_position_encoding.pystate"
 model.load_haiku_params(haiku_file)
 
-model_name = "classify_perceiver_io_learned_preprocessing.pth"
+model_name = "imagenet_learned_position_encoding.pth"
 
 state_dicts = {
     "model_state_dict": model.state_dict()
 }
 torch.save(state_dicts, os.path.join("pytorch_checkpoints", model_name))
 
-model = ClassificationPerceiver(prep_type="fourier_pos_encoding")
+model = ClassificationPerceiver(prep_type=PrepType.FOURIER_POS_PIXEL)
 haiku_file = "haiku_checkpoints/imagenet_fourier_position_encoding.pystate"
 model.load_haiku_params(haiku_file)
 
-model_name = "classify_perceiver_io_fourier_preprocessing.pth"
+model_name = "imagenet_fourier_position_encoding.pth"
 
 state_dicts = {
     "model_state_dict": model.state_dict()
