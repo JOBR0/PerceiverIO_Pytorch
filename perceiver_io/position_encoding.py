cb49f6f470cac20bc0befff27d81846d0154f5cf
Jonas <jonas.brokmeier@tum.de>
2022-01-31 03:26:17 +0100

    Remove header

--- perceiver_io/position_encoding.py
+++ perceiver_io/position_encoding.py
@@ -1,16 +1,3 @@
-# Copyright 2021 DeepMind Technologies Limited
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     https://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 """Position encodings and utilities."""
 
 import abc
@@ -214,15 +201,13 @@ class PositionEncodingProjector(AbstractPositionEncoding):
     def n_output_channels(self):
         return self._output_channels
 
-    def set_haiku_params(self, params, base_params = None):
+    def set_haiku_params(self, params, base_params=None):
         init_linear_from_haiku(self._projector, params.pop("linear"))
         if base_params is not None:
             self._base_position_encoding.set_haiku_params(base_params)
 
         if len(params) != 0:
             warnings.warn(f"Some parameters couldn't be matched to model: {params.keys()}")
-
-
 
 
 def build_position_encoding(
