d6b1ea83798fd15c2a68eb1b75fb2ad5502372d2
Jonas <jonas.brokmeier@tum.de>
2022-01-31 03:21:29 +0100

    Add unravel_index

--- utils/utils.py
+++ utils/utils.py
@@ -1,5 +1,5 @@
 import math
-from typing import Union, Sequence
+from typing import Union, Sequence, Tuple
 
 import torch
 from PIL import Image
@@ -24,6 +24,37 @@ def show_animation(images: np.ndarray, fps: int = 30):
     ani = ArtistAnimation(fig, frames, interval=interval, blit=True,
                                     repeat_delay=1000)
     plt.show()
+
+
+def unravel_index(
+    indices: torch.LongTensor,
+    shape: Tuple[int, ...],
+) -> torch.LongTensor:
+    r"""Converts flat indices into unraveled coordinates in a target shape.
+
+    This is a `torch` implementation of `numpy.unravel_index`.
+
+    From francois-rozet
+    https://github.com/pytorch/pytorch/issues/35674
+
+    Args:
+        indices: A tensor of indices, (*, N).
+        shape: The targeted shape, (D,).
+
+    Returns:
+        unravel coordinates, (*, N, D).
+    """
+
+    shape = torch.tensor(shape)
+    indices = indices % shape.prod()  # prevent out-of-bounds indices
+
+    coord = torch.zeros(indices.size() + shape.size(), dtype=int)
+
+    for i, dim in enumerate(reversed(shape)):
+        coord[..., i] = indices % dim
+        indices = indices // dim
+
+    return coord.flip(-1)
 
 
 def same_padding(input_size: Sequence[int], kernel_size: Union[int, Sequence[int]],
