d6303565b63d6a77e09f165f61402bab338a72e7
Jonas <jonas.brokmeier@tum.de>
2022-02-05 21:48:49 +0100

    Format img_classify

--- example_img_classify.py
+++ example_img_classify.py
@@ -1,17 +1,16 @@
 import os
 
 import cv2
-import torch
-
 import numpy as np
+import imageio
+import matplotlib.pyplot as plt
+
+import torch
+from torchvision import transforms
 
 from perceiver_io.classification_perceiver import ClassificationPerceiver, PrepType
 from utils.utils import load_image, dump_pickle
-
-from torchvision import transforms
-
 from utils.imagenet_labels import IMAGENET_LABELS
-import imageio
 
 import torch.nn.functional as F
 
@@ -25,18 +24,16 @@ img_size = (224, 224)
 
 normalize = transforms.Normalize(MEAN_RGB, STDDEV_RGB)
 
-
-prep_type = "fourier_pos_convnet"
-
+# There are three different pretrained models that use different input preprocessing:
 prep_type = PrepType.FOURIER_POS_CONVNET
-
-# TODO add explantations of prep_type
-
 if prep_type == PrepType.FOURIER_POS_CONVNET:
+    # Uses a 2D fourier position encoding and a 2D conv-net as preprocessing
     ckpt_file = "./pytorch_checkpoints/imagenet_conv_preprocessing.pth"
 elif prep_type == PrepType.LEARNED_POS_1X1CONV:
+    #  Uses a learned position encoding over the image and 1x1 convolution over the pixels
     ckpt_file = "./pytorch_checkpoints/imagenet_learned_position_encoding.pth"
 elif prep_type == PrepType.FOURIER_POS_PIXEL:
+    # Uses a 2D fourier position encoding and the raw pixels
     ckpt_file = "./pytorch_checkpoints/imagenet_fourier_position_encoding.pth"
 
 perceiver = ClassificationPerceiver(num_classes=1000,
@@ -53,7 +50,6 @@ checkpoint = torch.load(ckpt_file, map_location=device)
 
 perceiver.load_state_dict(checkpoint['model_state_dict'])
 
-
 # img = load_image("./sample_data/dalmation.jpg", device)
 #
 #
@@ -64,7 +60,7 @@ perceiver.load_state_dict(checkpoint['model_state_dict'])
 # min_size = min(h, w)
 # img_norm = transforms.functional.resized_crop(img, top=int(h/2-min_size/2), left=int(w/2-min_size/2), height=min_size, width=min_size, size=img_size)
 #
-# img_norm = normalize(img_norm)
+# img_norm2 = normalize(img_norm)
 
 with open("sample_data/dalmation.jpg", 'rb') as f:
     img = imageio.imread(f)
@@ -94,8 +90,6 @@ def resize_and_center_crop(image):
     return cv2.resize(image, (224, 224), interpolation=cv2.INTER_CUBIC)
 
 
-# Imagenet classification
-
 # Obtain a [224, 224] crop of the image while preserving aspect ratio.
 # With Fourier position encoding, no resize is needed -- the model can
 # generalize to image sizes it never saw in training
@@ -110,7 +104,6 @@ with torch.inference_mode():
     logits = perceiver(img_norm)
     # get top 5 predictions
     top_preds = torch.topk(logits, 5)[1]
-    # top_classes = top_preds[1]
 
     probs = F.softmax(logits, dim=-1).squeeze()
 
@@ -118,14 +111,13 @@ with torch.inference_mode():
     top_labels = np.array(IMAGENET_LABELS)[top_preds.cpu().numpy()]
     top_probs = probs[top_preds]
 
-    print(top_preds)
-    print(top_probs)
-    print(top_labels)
+
+print("Top 5 labels:")
+for i in range(top_probs.shape[1]):
+    print(f'{top_labels[0,i]}: {top_probs[0,i]}')
 
 dump_pickle(logits.numpy(), f"temp/output_{str(prep_type)}_torch.pickle")
 
-print(logits)
-
 # Show prediciton
-# plt.imshow(img[0].permute(1, 2, 0).cpu().numpy()/255)
-# plt.show()
+plt.imshow(img)
+plt.show()
