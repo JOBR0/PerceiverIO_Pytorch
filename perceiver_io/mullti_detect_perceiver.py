2714fe62aec2796ce5fddf05dc9f04e7943c05ae
JOBR0 <45601045+JOBR0@users.noreply.github.com>
2022-05-22 18:11:35 +0200

    Update mullti_detect_perceiver.py

--- perceiver_io/mullti_detect_perceiver.py
+++ perceiver_io/mullti_detect_perceiver.py
@@ -4,18 +4,13 @@ from typing import Sequence, List
 import torch.nn as nn
 import torch
 
-from perceiver_io.io_processors.postprocessors import ClassificationPostprocessor
+from perceiver_io.io_processors.postprocessors import ClassificationPostprocessor, ProjectionPostprocessor
 from perceiver_io.io_processors.preprocessors import ImagePreprocessor
 from perceiver_io.output_queries import TrainableQuery
 from perceiver_io.perceiver import PerceiverIO
 
 from perceiver_io.position_encoding import PosEncodingType
 
-
-class PrepType(Enum):
-    FOURIER_POS_CONVNET = 1
-    LEARNED_POS_1X1CONV = 2
-    FOURIER_POS_PIXEL = 3
 
 
 class MultiDetectPerceiver(nn.Module):
@@ -33,7 +28,8 @@ class MultiDetectPerceiver(nn.Module):
     """
 
     def __init__(self,
-                 num_classes: int = 1000,
+                 num_classes: int = 4,
+                 max_predictions: int = 200,
                  img_size: Sequence[int] = (224, 224),
                  img_channels: int = 3,
                  num_self_attends_per_block: int = 6,
@@ -43,41 +39,78 @@ class MultiDetectPerceiver(nn.Module):
                  ):
         super().__init__()
 
-        # TODO Change pos_embed dims
-        # TODO maybe make them equal to 244x224
-        # TODO pad inputs
+        # TYPE_VEHICLE = 1;
+        # TYPE_PEDESTRIAN = 2;
+        # TYPE_SIGN = 3;
+        # TYPE_CYCLIST = 4
 
-        img_preprocessor_big_h = ImagePreprocessor(
-            img_size=(164, 224),
+        # TODO (pad inputs)
+
+        self.num_classes = num_classes
+
+        small_resolution = (104, 224)
+        big_resolution = (164, 224)
+
+        img_preprocessor_img0 = ImagePreprocessor(
+            img_size=big_resolution,
             input_channels=img_channels,
-            position_encoding_type=PosEncodingType.FOURIER,
-            fourier_position_encoding_kwargs=dict(
-                concat_pos=True,
-                max_resolution=(41, 56),
-                num_bands=64,
-                sine_only=False
+            position_encoding_type=PosEncodingType.TRAINABLE,
+            trainable_position_encoding_kwargs=dict(
+                init_scale=0.02,
+                num_channels=258,
             ),
             prep_type="conv")
 
-        img_preprocessor_small_h = ImagePreprocessor(
-            img_size=(104, 224),
+        img_preprocessor_img1 = ImagePreprocessor(
+            img_size=big_resolution,
             input_channels=img_channels,
-            shared_conv_net=img_preprocessor_big_h.convnet,
-            position_encoding_type=PosEncodingType.FOURIER,
-            fourier_position_encoding_kwargs=dict(
-                concat_pos=True,
-                max_resolution=(56, 56),
-                num_bands=64,
-                sine_only=False
+            shared_conv_net=img_preprocessor_img0.convnet,
+            position_encoding_type=PosEncodingType.TRAINABLE,
+            trainable_position_encoding_kwargs=dict(
+                init_scale=0.02,
+                num_channels=258,
+            ),
+            prep_type="conv")
+
+        img_preprocessor_img2 = ImagePreprocessor(
+            img_size=small_resolution,
+            input_channels=img_channels,
+            shared_conv_net=img_preprocessor_img0.convnet,
+            position_encoding_type=PosEncodingType.TRAINABLE,
+            trainable_position_encoding_kwargs=dict(
+                init_scale=0.02,
+                num_channels=258,
+            ),
+            prep_type="conv")
+
+        img_preprocessor_img3 = ImagePreprocessor(
+            img_size=big_resolution,
+            input_channels=img_channels,
+            shared_conv_net=img_preprocessor_img0.convnet,
+            position_encoding_type=PosEncodingType.TRAINABLE,
+            trainable_position_encoding_kwargs=dict(
+                init_scale=0.02,
+                num_channels=258,
+            ),
+            prep_type="conv")
+
+        img_preprocessor_img4 = ImagePreprocessor(
+            img_size=small_resolution,
+            input_channels=img_channels,
+            shared_conv_net=img_preprocessor_img0.convnet,
+            position_encoding_type=PosEncodingType.TRAINABLE,
+            trainable_position_encoding_kwargs=dict(
+                init_scale=0.02,
+                num_channels=258,
             ),
             prep_type="conv")
 
         input_preprocessors = {
-            "img_0": img_preprocessor_big_h,
-            "img_1": img_preprocessor_big_h,
-            "img_2": img_preprocessor_small_h,
-            "img_3": img_preprocessor_big_h,
-            "img_4": img_preprocessor_small_h,
+            "img_0": img_preprocessor_img0,
+            "img_1": img_preprocessor_img1,
+            "img_2": img_preprocessor_img2,
+            "img_3": img_preprocessor_img3,
+            "img_4": img_preprocessor_img4,
         }
 
         perceiver_encoder_kwargs = dict(
@@ -91,15 +124,18 @@ class MultiDetectPerceiver(nn.Module):
         )
 
         output_query = TrainableQuery(
-            output_index_dims=num_classes,
+            output_index_dims=max_predictions,
             num_channels=1024,
             init_scale=0.02,
         )
 
-        output_postprocessor = ClassificationPostprocessor(
-            num_classes=num_classes,
-            num_input_channels=num_classes,
-            project=False
+        n_box_coordinates = 7 # [center_x, center_y, center_z, length, width, height, heading]
+        final_project_out_channels = 1000
+        n_predicted_values = 1 + num_classes + n_box_coordinates
+
+        output_postprocessor = ProjectionPostprocessor(
+            num_inputs=final_project_out_channels,
+            num_outputs=n_predicted_values
         )
 
         self.perceiver = PerceiverIO(
@@ -111,12 +147,16 @@ class MultiDetectPerceiver(nn.Module):
             perceiver_encoder_kwargs=perceiver_encoder_kwargs,
             output_queries=output_query,
             perceiver_decoder_kwargs=perceiver_decoder_kwargs,
-            final_project_out_channels=num_classes,
+            final_project_out_channels=final_project_out_channels,
             output_postprocessors=output_postprocessor)
 
-    def forward(self, img_list: List[torch.Tensor]):
+    def forward(self, input_dict):
         """
         :param img: (batch_size, 3, H, W)
         """
-        input_dict = {f"img_{i}": img for i, img in enumerate(img_list)}
-        return self.perceiver(input_dict)
+        #input_dict = {f"img_{i}": img for i, img in enumerate(img_list)}
+        outputs = self.perceiver(input_dict)
+        class_predictions = outputs[:, :, :self.num_classes + 1]
+        box_predictions = outputs[:, :, self.num_classes + 1:]
+
+        return class_predictions, box_predictions
